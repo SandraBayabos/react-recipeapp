aa9c83cd3372cfbcdf75ca53629d2bce82c84e95
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-09-17 18:14:51 +0800

    created function to call API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "c5891d4d";
   const APP_KEY = "a6a8cc744e5f1fb0719a8bff864e6060	";
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  // const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-  const [counter, setCounter] = useState(0);
+  //sets a state for recipes
+  const [recipes, setReceipes] = useState([]);
 
-  //useEffect is a Hook - tells React that your component needs to do something after the render. React will remember the function you passed (i.e. the effect you passed) and call it later after performing the DOM update.
+  // const [counter, setCounter] = useState(0);
+
+  //useEffect is a Hook - tells React that your component needs to do something after the render. React will remember the function you passed (i.e. the effect you passed, which is the [counter]) and call it later after performing the DOM update.
   useEffect(() => {
-    console.log("Effect has been run");
-  }, [counter]);
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setReceipes(data.hits);
+    console.log(data.hits);
+  };
 
   return (
     <div className="App">
@@ -21,7 +34,13 @@ const App = () => {
           Search
         </button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      {recipes.map(recipe => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
