c5dcda7cdcb4a6e0071dbc599d477e27c4308ab6
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-09-17 18:45:18 +0800

    edited component to add recipe and styled page

--- src/App.js
+++ src/App.js
@@ -5,40 +5,62 @@ import "./App.css";
 const App = () => {
   const APP_ID = "c5891d4d";
   const APP_KEY = "a6a8cc744e5f1fb0719a8bff864e6060	";
-  // const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  ////////////////////STATE/////////////////
 
   //sets a state for recipes
   const [recipes, setReceipes] = useState([]);
-
-  // const [counter, setCounter] = useState(0);
+  //sets state for the search bar
+  const [search, setSearch] = useState("");
+  //sets state for the button i.e. only when click button then fetches the data for you
+  const [query, setQuery] = useState("chicken");
 
   //useEffect is a Hook - tells React that your component needs to do something after the render. React will remember the function you passed (i.e. the effect you passed, which is the [counter]) and call it later after performing the DOM update.
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
+  //getQuery only runs once we have clicked on the Search button. i.e. useEffect only runs once our query changes
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query} &app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setReceipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = e => {
+    //value of the input
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
       {recipes.map(recipe => (
         <Recipe
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
         />
       ))}
     </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { isRegExp } from "util";
 
 //passing down title, calories and image as props
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
       <p>{calories}</p>
       <img src={image} alt="" />
     </div>
