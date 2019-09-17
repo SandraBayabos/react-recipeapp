10e3fd0093506a3bf37055c5ecc639cfb95db7da
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-09-17 19:46:53 +0800

    added styling module

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { isRegExp } from "util";
+import style from "./recipe.module.css";
 
 //passing down title, calories and image as props
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className={style.recipe}>{title}</h1>
       <ol>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
